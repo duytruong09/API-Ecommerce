2705c4fe03ba4d7869ff177982b77a7d398dae4d
duytruong09 <75570501+duytruong09@users.noreply.github.com>
2022-01-03 21:33:26 +0700

    First commit

---
+++ index.js
@@ -0,0 +1,22 @@
+const express = require('express')
+const app = express()
+const mongoose = require('mongoose')
+const dotenv = require('dotenv').config()
+const userRoute = require("./routes/user")
+const authRoute = require("./routes/auth")
+
+mongoose
+    .connect(process.env.MONGO_URL)
+    .then(() => console.log("DBConnection Successfully!"))
+    .catch((err) => {
+        console.log(err);
+    });
+
+app.use(express.json())
+app.use('/api/auth', authRoute)
+app.use('/api/users', userRoute)
+
+
+app.listen(process.env.PORT || 5000, () => {
+    console.log(`Server is running!`);
+});
